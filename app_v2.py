50a8571db1271c5c498dafa174f9b004edfe6fed
Shoeb Shaikh <90shoeb@gmail.com>
2022-06-09 10:59:40 +0800

    changed app to give appropriate output

--- app_v2.py
+++ app_v2.py
@@ -29,17 +29,18 @@ matplotlib.use('Agg')
 def process_csv(csv_file_buffer, mode):
         df = pd.read_csv(csv_file_buffer)
         # Creating a text element to let the reader know that the data is loading.
-        data_load_state = st.text('Loading data...')
+        st.text('Loading data...')
         df_replaceNAN = df.replace(np.nan, 'NA', regex=True)
-        processed_dataset = df_replaceNAN[['ATS', 'CMD', 'Problem Code', 'Cause Code', 'Remedy Code']].to_numpy()
+        df_replaceNAN['CMD'] = df_replaceNAN['CMD'].apply(lambda x:str(x).lower())
+        df_replaceNAN['PCR'] = df_replaceNAN[['Problem Code','Cause Code', 'Remedy Code']].apply(lambda x:('-').join(x.astype(str)),axis=1)
+        processed_dataset = df_replaceNAN[['CMD','PCR']].to_numpy()
         te = TransactionEncoder()
         te_ary = te.fit(processed_dataset).transform(processed_dataset)
         transformed_dataset = pd.DataFrame(te_ary, columns=te.columns_)
-
         # Notifying the reader that the data was successfully loaded.
-        data_load_state.text('Loading data...done!')
+        st.text('Loading data...done!')      
 
-        return transformed_dataset
+        return transformed_dataset,df_replaceNAN
 
 def runApriori(dataset):
         start = datetime.now()
@@ -52,14 +53,15 @@ def runApriori(dataset):
         st.success("Completed in {} seconds".format(deltatime.seconds + deltatime.microseconds / 1000000))
         return association_rule_ap
 
-def runFPgrowth(dataset):
+def runFPgrowth(dataset,df_replaceNaN):
         start = datetime.now()
-        frequent_itemsets_fp = fpgrowth(dataset,min_support=0.1 ,use_colnames=True) 
+        frequent_itemsets_fp = fpgrowth(dataset,min_support=0.01 ,use_colnames=True) 
         deltatime = datetime.now() - start
         frequent_itemsets_fp.sort_values(by='support',ascending=False,inplace=True)
         association_rule_fp = association_rules(frequent_itemsets_fp,metric='confidence',min_threshold=0.1) 
         association_rule_fp.sort_values(by='leverage',ascending=False,inplace=True)    
         association_rule_fp['length'] = association_rule_fp['consequents'].apply(lambda x: len(x))
+        association_rule_fp = association_rule_fp[association_rule_fp.consequents.astype(str).str.contains('|'.join(df_replaceNaN.PCR.unique().tolist()))] 
         deltatime = datetime.now() - start
         st.success("Completed in {} seconds".format(deltatime.seconds + deltatime.microseconds / 1000000))
         return association_rule_fp
@@ -164,8 +166,9 @@ if __name__ == "__main__":
                 csv_file_buffer = st.file_uploader("Upload the work order file (.csv)", type=["csv"], key="train_upload")
 
                 if csv_file_buffer is not None:
-                        dataset = process_csv(csv_file_buffer, mode)
-
+                        dataset,df_replaceNAN = process_csv(csv_file_buffer, mode)
+                        st.write(dataset.head())
+                        st.write(df_replaceNAN.head())
                 
                 st.subheader("Generate Rules")
 
@@ -175,7 +178,7 @@ if __name__ == "__main__":
                         if types_of_algos == "Apriori":
                                 session_state.association_rule = runApriori(dataset)
                         elif types_of_algos == "FPgrowth":
-                                session_state.association_rule = runFPgrowth(dataset)
+                                session_state.association_rule = runFPgrowth(dataset,df_replaceNAN)
                         elif types_of_algos == "FPmax":
                                 session_state.association_rule = runFPmax(dataset)
 
@@ -209,29 +212,33 @@ if __name__ == "__main__":
                                 label = "Input codes",
                                 key = "codes")
 
-                pType = st.selectbox(label = "Association you want to determine", options = ['antecedents', 'consequents'], key="pType")
+                pType = 'consequents'#st.selectbox(label = "Association you want to determine", options = ['antecedents', 'consequents'], key="pType")
 
                 # codes = frozenset([str(s) for s in pInput.split(',')])
-
 
                 metrics = st.checkbox("Additional options", key="metrics")
                 
                 if metrics:
                         support, confidence, lift = process_metrics()
                 else:
-                        support, confidence, lift = 0.3,0.7,1.2
+                        support, confidence, lift = 0.01,0.01,0.01
 
                 if st.button("Submit"):
                         
                         # load data as hdf5
                         # association_rules = pd.read_hdf('models/association_rule.h5')
                         association_rules = pd.read_csv('models/association_rule.csv')
-                        codes_parsed = [str(s) for s in pInput.split(',')]
-                        codes_parsed = ['Replace', 'NA']
-                        st.info(codes_parsed)
+                        association_rules = association_rules.reset_index(drop=True)
+                        
+                        codes_parsed = [pInput.strip().lower()]
+                        # codes_parsed = [str(s) for s in pInput.split(',')]
+                        # codes_parsed = ['Replace', 'NA']
+                        st.info(pInput)
                         codes = pd.Series([codes_parsed] * len(association_rules), index = association_rules.index)
-                        st.dataframe(association_rules.head(5))
-                        st.info(association_rules[association_rules['consequents'] == codes])
+                        codes = codes.astype(str)
+                    
+                        # st.dataframe(association_rules.head(5))
+                        # st.info(association_rules[association_rules['consequents'] == codes])
 
                         if pType == "antecedents":
                                 df = association_rules[(association_rules['consequents'] == codes) & (association_rules['support'] > support) & (association_rules['confidence'] > confidence) & (association_rules['lift'] > lift)]
